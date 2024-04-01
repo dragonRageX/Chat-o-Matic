afe1b8d5acc47797d25d8a14b5495aa482a96e90
param <param772003@gmail.com>
2024-04-01 19:41:43 +0530

    using gql and useQuery to get messages

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,15 +1,45 @@
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
+    uri: 'http://localhost:4000/graphql',
     cache: new InMemoryCache(),
 });
+
+const GET_MESSAGES = gql`
+query {
+    messages {
+      id
+      content
+      user
+    } 
+  }
+`;
+
+function Messages({ user })
+{
+    const { loading, error, data } = useQuery(GET_MESSAGES);
+
+    if(loading)
+    {
+        return <p>Loading...</p>;
+    }
+    if(error)
+    {
+        return <pre>Error: {error.message}</pre>;
+    }
+    else
+    {
+        return JSON.stringify(data);
+    }
+}
 
 function Chat()
 {
     return (
-        <div>I'm a Chat Window</div>
+        <div>
+            <Messages user="Jack" />
+        </div>
     );
 }
 
